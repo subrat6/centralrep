b6c4a9e3020a8bb787243524515c5712260e4980
BUNTY-PC\BUNTY <princesubu.37@gmail.com>
2017-09-07 16:08:47 +0530

    Commit Calculator

---
+++ HelloWorld/src/com/hw/beans/Calculator.java
@@ -0,0 +1,18 @@
+package com.hw.beans;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int arr[] = new int[10];
+		int j = 1;
+		while (j <= 10) {
+			for (int i = 1; i <= arr.length; i++) {
+				int total = j * i;
+				System.out.println("total = " + j + "*" + i + " = " + total);
+			}
+			System.out.println("new------------------------------------------------new");
+			j++;
+		}
+	}
+
+}
